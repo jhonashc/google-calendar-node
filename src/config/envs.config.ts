064f177a77f5ac49f8c9f1afd7615b90a5862408
jhonashc <jhonhuiracocha123456789@gmail.com>
2024-08-24 13:50:14 -0500

    envs config added

--- src/config/envs.config.ts
+++ src/config/envs.config.ts
@@ -2,6 +2,9 @@ import { envsSchema } from '../schemas';
 
 const envServer = envsSchema.safeParse({
   PORT: process.env.PORT,
+  CLIENT_ID: process.env.CLIENT_ID,
+  CLIENT_SECRET: process.env.CLIENT_SECRET,
+  REDIRECT_URL: process.env.REDIRECT_URL,
 });
 
 if (!envServer.success) {
